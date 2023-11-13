10e6898e5592d40a77aee176a2734251c49a217e
Ketch4n <mangao.christian.04@gmail.com>
2023-11-13 16:32:10 +0800

    accomplishment

---
+++ attendance_monitoring.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2023 at 09:30 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `attendance_monitoring`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accomplishment`
+--
+
+CREATE TABLE `accomplishment` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `section_id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `name`, `user_id`, `role`) VALUES
+(40, 'admin@gmail.com', '$2y$10$IrCl7lOiuLtO29tF0b0zVuguirvGuL.d8Wm8R0F0AiMntVXRdy6oO', 'Mr. Ketchan', '152966493', 'Admin'),
+(41, 'estab@gmail.com', '$2y$10$S5d8iiye5NYJ917hDrmZRebWAO27oAey5xz7v1G.1U1N575i9HGfK', 'Mr. Night Tech', '962067827', 'Establishment');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(255) NOT NULL,
+  `section_id` int(255) NOT NULL,
+  `student_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dtr`
+--
+
+CREATE TABLE `dtr` (
+  `id` int(255) NOT NULL,
+  `student_id` int(255) NOT NULL,
+  `estab_id` int(255) NOT NULL,
+  `time_in_am` time NOT NULL,
+  `in_am` varchar(2) NOT NULL,
+  `loc_in_am` varchar(255) NOT NULL,
+  `time_out_am` time NOT NULL,
+  `out_am` varchar(2) NOT NULL,
+  `loc_out_am` varchar(255) NOT NULL,
+  `time_in_pm` time NOT NULL,
+  `in_pm` varchar(2) NOT NULL,
+  `loc_in_pm` varchar(255) NOT NULL,
+  `time_out_pm` time NOT NULL,
+  `out_pm` varchar(2) NOT NULL,
+  `loc_out_pm` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dtr`
+--
+
+INSERT INTO `dtr` (`id`, `student_id`, `estab_id`, `time_in_am`, `in_am`, `loc_in_am`, `time_out_am`, `out_am`, `loc_out_am`, `time_in_pm`, `in_pm`, `loc_in_pm`, `time_out_pm`, `out_pm`, `loc_out_pm`, `date`) VALUES
+(41, 45, 5, '03:16:00', 'PM', '', '00:00:00', '--', '', '00:00:00', '--', '', '00:00:00', '--', '', '2023-11-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `establishment`
+--
+
+CREATE TABLE `establishment` (
+  `id` int(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `establishment_name` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `creator_id` int(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `establishment`
+--
+
+INSERT INTO `establishment` (`id`, `code`, `establishment_name`, `location`, `creator_id`, `status`) VALUES
+(5, 'dfd1yeXO', 'NITEX', '8.4934431123.7858202', 41, 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(255) NOT NULL,
+  `establishment_id` int(255) NOT NULL,
+  `student_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `id` int(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `section_name` varchar(255) NOT NULL,
+  `admin_id` int(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `section`
+--
+
+INSERT INTO `section` (`id`, `code`, `section_name`, `admin_id`, `status`) VALUES
+(9, 'W430WhWk', 'EAGLE - 4A', 40, 'Active'),
+(11, 'RFJ3SiSg', 'RUBY - 2A', 40, 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `name`, `user_id`, `role`) VALUES
+(45, 'mangao.christian.04@gmail.com', '$2y$10$KgPf3VhuYR9ke74GjAKgJOCmh5f/e5wgpn41v/gUpyHKXUEhpPYPy', 'Ketchan', '294313492', 'Student');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accomplishment`
+--
+ALTER TABLE `accomplishment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dtr`
+--
+ALTER TABLE `dtr`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `establishment`
+--
+ALTER TABLE `establishment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accomplishment`
+--
+ALTER TABLE `accomplishment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `dtr`
+--
+ALTER TABLE `dtr`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `establishment`
+--
+ALTER TABLE `establishment`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `section`
+--
+ALTER TABLE `section`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ users/student/upload.php
@@ -0,0 +1,40 @@
+<?php
+include '../../db/database.php';
+
+// Assuming you have a database connection established
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the values from the request body
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    $email = $data['email'];
+    $section = $data['section'];
+    $comment = $data['comment'];
+    $date = $data['date'];
+
+    // Directly insert into the reference table
+    $sqlInsert = "INSERT INTO accomplishment (email, section_id, comment, date) VALUES (?, ?, ?, ?)";
+    $stmtInsert = $con->prepare($sqlInsert);
+    $stmtInsert->bind_param("ssss", $email, $section, $comment, $date);
+
+    if ($stmtInsert->execute()) {
+        // Data inserted successfully
+        $response = array('status' => 'Success', 'message' => "Accomplishment added successfully");
+        echo json_encode($response);
+    } else {
+        // Error inserting data
+        $response = array('status' => 'error', 'message' => "Error adding accomplishment");
+        echo json_encode($response);
+    }
+} else {
+    // Invalid request method
+    $response = array('status' => 'error', 'message' => 'Invalid request method');
+    echo json_encode($response);
+}
+
+header('Content-Type: application/json');
+
+// Close the database connection
+$con->close();
+?>
